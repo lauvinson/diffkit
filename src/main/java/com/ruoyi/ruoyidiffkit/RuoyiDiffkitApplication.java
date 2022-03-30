529358fb5b268d773a972a34a1301b3910c39228
vinson <liuws@zhiyun-tech.com>
2022-03-30 15:09:31 +0800

    init

---
+++ src/main/java/com/ruoyi/ruoyidiffkit/RuoyiDiffkitApplication.java
@@ -0,0 +1,13 @@
+package com.ruoyi.ruoyidiffkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiDiffkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoyiDiffkitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruoyi/ruoyidiffkit/RuoyiDiffkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruoyi.ruoyidiffkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuoyiDiffkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
